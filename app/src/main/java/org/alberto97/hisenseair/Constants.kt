8908a8232ef04e3449bff6a94485f95227aab7d1
Alberto Pedron <albertop2197@gmail.com>
2020-10-02 21:00:05 +0200

    Delete old preference-based Device Control UI

--- app/src/main/java/org/alberto97/hisenseair/Constants.kt
+++ app/src/main/java/org/alberto97/hisenseair/Constants.kt
@@ -6,24 +6,6 @@ object BottomSheetFragments {
     const val TEMP = "TempSheet"
 }
 
-object PreferenceConstants {
-    const val PREFERENCE_AMBIENT_TEMP = "ambientTemp"
-    const val PREFERENCE_TEMP_CONTROL = "tempControl"
-    const val PREFERENCE_MODE = "mode"
-    const val PREFERENCE_FAN_SPEED = "fan"
-    const val PREFERENCE_NIGHT = "night"
-    const val PREFERENCE_SETTINGS = "settings"
-    const val PREFERENCE_POWER = "power"
-    const val PREFERENCE_CATEGORY_AIRFLOW = "airFlow"
-    const val PREFERENCE_AIRFLOW_HORIZONTAL = "horizontal"
-    const val PREFERENCE_AIRFLOW_VERTICAL = "vertical"
-    const val PREFERENCE_CATEGORY_ADVANCED = "advanced"
-    const val PREFERENCE_ADVANCED_BACKLIGHT = "backlight"
-    const val PREFERENCE_ADVANCED_ECO = "eco"
-    const val PREFERENCE_ADVANCED_QUIET = "quiet"
-    const val PREFERENCE_ADVANCED_BOOST = "boost"
-}
-
 object UIConstants {
     /*
      * https://github.com/aosp-mirror/platform_frameworks_base/blob/android11-release/packages/SystemUI/src/com/android/systemui/controls/ui/DetailDialog.kt#L49
