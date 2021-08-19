2665563bb153f16c5fdb2bf6a418f96e86d1f636
Alberto Pedron <albertop2197@gmail.com>
2021-08-19 18:29:06 +0200

    TemperatureControlStep: Use Row instead of ConstraintLayout It was overkill and not stable yet

--- app/src/main/java/org/alberto97/hisenseair/ui/devicecontrol/TemperatureControlStep.kt
+++ app/src/main/java/org/alberto97/hisenseair/ui/devicecontrol/TemperatureControlStep.kt
@@ -1,6 +1,8 @@
 package org.alberto97.hisenseair.ui.devicecontrol
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
@@ -11,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import org.alberto97.hisenseair.ui.theme.AppTheme
 
 @Composable
@@ -21,20 +22,13 @@ fun TemperatureControlStep(
     onTempUp: () -> Unit,
     onTempClick: () -> Unit
 ) {
-    ConstraintLayout(
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
         modifier = Modifier.fillMaxWidth().padding(horizontal = 14.dp, vertical = 24.dp)
     ) {
-        val (buttonLeft, buttonRight, text) = createRefs()
 
         IconButton(
             onClick = { onTempDown() },
-            modifier = Modifier
-                .constrainAs(buttonLeft) {
-                    top.linkTo(parent.top)
-                    bottom.linkTo(parent.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(text.start)
-                }
         ) {
             Icon(
                 Icons.Filled.ChevronLeft,
@@ -46,22 +40,9 @@ fun TemperatureControlStep(
             text = " $temp°",
             style = MaterialTheme.typography.h4,
             modifier = Modifier.clickable(onClick = onTempClick)
-                .constrainAs(text) {
-                    top.linkTo(parent.top)
-                    bottom.linkTo(parent.bottom)
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
         )
         IconButton(
             onClick = { onTempUp() },
-            modifier =  Modifier
-                .constrainAs(buttonRight) {
-                    top.linkTo(parent.top)
-                    bottom.linkTo(parent.bottom)
-                    start.linkTo(text.end)
-                    end.linkTo(parent.end)
-                }
         ) {
             Icon(
                 Icons.Filled.ChevronRight,
