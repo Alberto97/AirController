aba35606590d0abf7e27b464de5b107b05ebc8f5
Alberto Pedron <albertop2197@gmail.com>
2021-09-09 18:14:25 +0200

    Add Koin helper to retrieve ViewModels bounded to navgraph lifecycle

---
+++ app/src/main/java/org/alberto97/hisenseair/koin/ViewModelComposeExt.kt
@@ -0,0 +1,23 @@
+package org.alberto97.hisenseair.koin
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavBackStackEntry
+import org.koin.androidx.viewmodel.ext.android.getViewModel
+import org.koin.core.parameter.ParametersDefinition
+import org.koin.core.qualifier.Qualifier
+
+/*
+ * From https://github.com/InsertKoinIO/koin/issues/1079#issuecomment-902215765
+ */
+@Composable
+inline fun <reified VM : ViewModel> getNavGraphViewModel(
+    qualifier: Qualifier? = null,
+    noinline parameters: ParametersDefinition? = null,
+    backStackEntry: NavBackStackEntry
+): VM {
+    return remember(qualifier,parameters) {
+        backStackEntry.getViewModel(qualifier, parameters)
+    }
+}
