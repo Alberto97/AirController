18cfddda9474e9efca6e1537ac250fda0010611d
Alberto Pedron <albertop2197@gmail.com>
2021-09-12 19:37:03 +0200

    ResultWrapper: Add code field

--- app/src/main/java/org/alberto97/hisenseair/models/ResultWrapper.kt
+++ app/src/main/java/org/alberto97/hisenseair/models/ResultWrapper.kt
@@ -2,9 +2,10 @@ package org.alberto97.hisenseair.models
 
 sealed class ResultWrapper<T>(
     val data: T? = null,
+    val code: Int? = null,
     val message: String = ""
 ) {
     class Success<T>(data: T) : ResultWrapper<T>(data)
     //class Loading<T>(data: T? = null) : ResultWrapper<T>(data)
-    class Error<T>(message: String, data: T? = null) : ResultWrapper<T>(data, message)
+    class Error<T>(message: String, code: Int? = null, data: T? = null) : ResultWrapper<T>(data, code, message)
 }
