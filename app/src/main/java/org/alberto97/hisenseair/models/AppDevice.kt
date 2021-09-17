0e365bcc977a0dc8f47341461c0858eb6f1b4a03
Alberto Pedron <albertop2197@gmail.com>
2021-09-17 15:25:25 +0200

    model: Make AppDevice and AppDeviceState data classes

--- app/src/main/java/org/alberto97/hisenseair/models/AppDevice.kt
+++ app/src/main/java/org/alberto97/hisenseair/models/AppDevice.kt
@@ -1,3 +1,10 @@
 package org.alberto97.hisenseair.models
 
-class AppDevice(val id: String, val name: String, val available: Boolean, val lanIp: String, val mac: String, val ssid: String?)
+data class AppDevice(
+    val id: String,
+    val name: String,
+    val available: Boolean,
+    val lanIp: String,
+    val mac: String,
+    val ssid: String?
+)
--- app/src/main/java/org/alberto97/hisenseair/models/AppDeviceState.kt
+++ app/src/main/java/org/alberto97/hisenseair/models/AppDeviceState.kt
@@ -3,24 +3,24 @@ package org.alberto97.hisenseair.models
 import org.alberto97.hisenseair.features.*
 
 
-class AppDeviceState {
-    var productName: String = "Device"
-    var backlight: Boolean = false
-    var supportedModes = emptyList<WorkMode>()
-    var workMode: WorkMode = WorkMode.Auto
-    var horizontalAirFlow: Boolean = false
-    var verticalAirFlow: Boolean = false
-    var quiet: Boolean = false
-    var eco: Boolean = false
-    var boost: Boolean = false
-    var sleepMode: SleepMode = SleepMode.OFF
-    var supportedSleepModes = emptyList<SleepModeData>()
-    var supportedSpeeds = emptyList<FanSpeed>()
-    var fanSpeed: FanSpeed = FanSpeed.Auto
-    var temp: Int = 0
-    var roomTemp: Int = 0
-    var tempUnit: TempType = TempType.Fahrenheit
-    var on: Boolean = false
-    var maxTemp = 90
-    var minTemp = 61
-}
+data class AppDeviceState(
+    var productName: String = "Device",
+    var backlight: Boolean = false,
+    var supportedModes: List<WorkMode> = emptyList(),
+    var workMode: WorkMode = WorkMode.Auto,
+    var horizontalAirFlow: Boolean = false,
+    var verticalAirFlow: Boolean = false,
+    var quiet: Boolean = false,
+    var eco: Boolean = false,
+    var boost: Boolean = false,
+    var sleepMode: SleepMode = SleepMode.OFF,
+    var supportedSleepModes: List<SleepModeData> = emptyList(),
+    var supportedSpeeds: List<FanSpeed> = emptyList(),
+    var fanSpeed: FanSpeed = FanSpeed.Auto,
+    var temp: Int = 0,
+    var roomTemp: Int = 0,
+    var tempUnit: TempType = TempType.Fahrenheit,
+    var on: Boolean = false,
+    var maxTemp: Int = 90,
+    var minTemp: Int = 61
+)
