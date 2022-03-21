834400858ef81aa682d29692c653da19b93b07ee
Alberto Pedron <albertop2197@gmail.com>
2022-03-21 23:20:34 +0100

    ayla: Document known response codes from the WS

--- provider-ayla/src/main/java/org/alberto97/aircontroller/provider/ayla/internal/network/api/AylaLogin.kt
+++ provider-ayla/src/main/java/org/alberto97/aircontroller/provider/ayla/internal/network/api/AylaLogin.kt
@@ -8,9 +8,20 @@ import retrofit2.http.POST
 
 internal interface AylaLogin {
 
+    /**
+     * Possible HTTP response codes:
+     * 200: OK (If user successfully logged in)
+     * 401: Unauthorized (If app_id and app_secret do not match OR if email and password do not match, etc.)
+     * 404: The app_id or app_secret are invalid.
+     */
     @POST("sign_in.json")
     suspend fun login(@Body login: Login): LoginOutput
 
+    /**
+     * Possible HTTP response codes:
+     * 200: OK (If new access token was successfully generated)
+     * 401: Unauthorized (If refresh token does not match current user)
+     */
     @POST("refresh_token.json")
     suspend fun refreshToken(@Body login: LoginRefresh): LoginOutput
 }
--- provider-ayla/src/main/java/org/alberto97/aircontroller/provider/ayla/internal/network/api/AylaService.kt
+++ provider-ayla/src/main/java/org/alberto97/aircontroller/provider/ayla/internal/network/api/AylaService.kt
@@ -7,10 +7,24 @@ import retrofit2.http.*
 
 internal interface AylaService {
 
+    /**
+     * Retrieve a list of all devices for the user, with some details.
+     * Possible HTTP response codes:
+     * 200: OK
+     * 401: Unauthorized (If app_id and app_secret do not match OR if email and password do not match, etc.)
+     */
     @Authorized
     @GET("devices.json")
     suspend fun getDevices(): List<Device.Wrapper>
 
+    /**
+     * Register a new device for the specified user.
+     * Possible HTTP response codes:
+     * 201: Created
+     * 401: Unauthorized
+     * 404: Unable to find registrable device with user ID
+     * 417: User ID is nil or device registration candidate is invalid
+     */
     @Authorized
     @POST("devices.json")
     suspend fun postDevice(@Body device: SetupDevice): Device.Wrapper
@@ -24,6 +38,13 @@ internal interface AylaService {
         setupToken: String?
     ): ConnectedDevice.Wrapper
 
+    /**
+     * Unregister a device from the current user.
+     * Possible HTTP response codes:
+     * 200: OK
+     * 401: Unauthorized
+     * 404: Not Found
+     */
     @Authorized
     @DELETE("devices/{key}.json")
     suspend fun deleteDevice(
@@ -31,6 +52,13 @@ internal interface AylaService {
         key: Int
     ): Response<Unit>
 
+    /**
+     * Fetch device details specified by Device Serial Number (DSN).
+     * Possible HTTP response codes:
+     * 200: OK (if user successfully logged in)
+     * 400: Invalid Parameters Supplied
+     * 401: Unauthorized
+     */
     @Authorized
     @GET("dsns/{dsn}.json")
     suspend fun getDevice(
@@ -38,6 +66,14 @@ internal interface AylaService {
         dsn: String
     ): Device.Wrapper
 
+    /**
+     * Update the display name of the device.
+     * Possible HTTP response codes:
+     * 200: OK (if user successfully logged in)
+     * 401: Unauthorized
+     * 403: Forbidden
+     * 404: DSN with provided ID Not Found
+     */
     @Authorized
     @PUT("dsns/{dsn}.json")
     suspend fun putDevice(
@@ -48,6 +84,13 @@ internal interface AylaService {
         deviceObj: ProductName
     )
 
+    /**
+     * Retrieve all the properties for a specified device serial number (DSN).
+     * Possible HTTP response codes:
+     * 200: OK
+     * 401: Unauthorized
+     * 404: Not Found
+     */
     @Authorized
     @GET("dsns/{dsn}/properties.json")
     suspend fun getDeviceProperties(
@@ -55,6 +98,13 @@ internal interface AylaService {
         dsn: String
     ): List<Property.Wrapper>
 
+    /**
+     * Retrieve details about a device property on a specific device.
+     * Possible HTTP response codes:
+     * 200: OK
+     * 401: Unauthorized
+     * 404: Not Found
+     */
     @Authorized
     @GET("dsns/{dsn}/properties/{property}.json")
     suspend fun getDeviceProperty(
@@ -65,6 +115,14 @@ internal interface AylaService {
         property: String
     ): Property.Wrapper
 
+    /**
+     * Create one datapoint for a property on the specified device.
+     * Possible HTTP response codes:
+     * 201: Created
+     * 401: Unauthorized
+     * 403: Forbidden
+     * 404: Not Found
+     */
     @Authorized
     @POST("dsns/{dsn}/properties/{property}/datapoints.json")
     suspend fun setDeviceProperty(
