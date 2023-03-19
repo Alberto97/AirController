e099efc3c30fe2402fae281b675041a44c68f052
Alberto Pedron <albertop2197@gmail.com>
2023-03-19 15:47:20 +0100

    ui: splash: Remove unused imports

--- app/src/main/java/org/alberto97/aircontroller/ui/splash/SplashViewModel.kt
+++ app/src/main/java/org/alberto97/aircontroller/ui/splash/SplashViewModel.kt
@@ -1,9 +1,6 @@
 package org.alberto97.aircontroller.ui.splash
 
 import android.app.Application
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
